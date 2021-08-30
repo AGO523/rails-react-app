4b7d88061fd5e73628324ad3d2ba6b2b9d691b2d
AGO523 <unpouletfrais0523@gmail.com>
2021-08-31 05:44:08 +0900

    投稿画面の説明を修正

--- frontend/src/components/post/PostCard.tsx
+++ frontend/src/components/post/PostCard.tsx
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { blue } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
   root: {
@@ -20,6 +21,9 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+  mainTitle: {
+    color: "palevioletred",
+  }
 });
 
 export default function OutlinedCard() {
@@ -28,17 +32,17 @@ export default function OutlinedCard() {
   return (
     <Card className={classes.root} variant="outlined">
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          学習内容の紹介
-        </Typography>
-        <Typography variant="h5" component="h2">
-          色々な学習があります
+        <Typography className={classes.mainTitle} variant="h5" component="h2">
+          学習の広場
         </Typography>
         <Typography variant="body2" component="p">
           ・先生たちと一緒に学習する内容を知ることができます。
         </Typography>
         <Typography variant="body2" component="p">
           ・気になる先生のページに行って、その先生の詳しいことを知ることができます。
+        </Typography>
+        <Typography variant="body2" component="p">
+          ・先生に学んだことを知らせることができます。
         </Typography>
       </CardContent>
     </Card>
