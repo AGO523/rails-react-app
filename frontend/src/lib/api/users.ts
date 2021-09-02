0df41c79d7a7e7454b13995a318433c89e17827f
AGO523 <unpouletfrais0523@gmail.com>
2021-09-03 05:14:25 +0900

    ユーザデータがコンソールに表示されるよう変更

--- frontend/src/lib/api/users.ts
+++ frontend/src/lib/api/users.ts
@@ -14,6 +14,18 @@ export const getUsers = () => {
   })
 }
 
+// ユーザーを個別に取得
+export const getUserPages = (id: number) => {
+  return client.get(`user_pages/${id}`, {
+    headers: {
+      "access-token": Cookies.get("_access_token"),
+      "client": Cookies.get("_client"),
+      "uid": Cookies.get("_uid")
+    }
+  })
+}
+
+
 // id指定でユーザー情報を個別に取得
 export const getUser = (id: number | undefined) => {
   return client.get(`users/${id}`)
